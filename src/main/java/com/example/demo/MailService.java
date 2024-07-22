220f47484e84fdcc79d5b5ca5dfcb3b1365e11dc
nacho <naachoperez11@gmail.com>
2024-07-21 21:03:45 -0300

    hotmail

--- src/main/java/com/example/demo/MailService.java
+++ src/main/java/com/example/demo/MailService.java
@@ -13,7 +13,7 @@ public class MailService {
 
     public void sendEmail(UserModel userModel) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo("arquilarisaaimar@hotmail.com");
+        message.setTo("naachoperez1@hotmail.com");
         message.setSubject("Nuevo contacto del chatbot");
         message.setText("Comunicarse con: " + userModel.getNombre() + ",\n"
                 + "Teléfono: " + userModel.getTelefono() + ",\n"
