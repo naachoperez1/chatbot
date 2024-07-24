62cec8d36d3ef8d19b34be9120fba3b6ef5b96fb
nacho <naachoperez11@gmail.com>
2024-07-24 19:30:17 -0300

    gmail

--- src/main/java/com/example/demo/MailService.java
+++ src/main/java/com/example/demo/MailService.java
@@ -13,7 +13,7 @@ public class MailService {
 
     public void sendEmail(UserModel userModel) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo("info@grupoinhaut.com");
+        message.setTo("pablofiguerero88@gmail.com");
         message.setSubject("Nuevo contacto del chatbot");
         message.setText("Comunicarse con: " + userModel.getNombre() + ",\n"
                 + "Teléfono: " + userModel.getTelefono() + ",\n"
