3f47f3d94fa2f0a2b86996f5b6a397d68f3fe867
nacho <naachoperez11@gmail.com>
2024-07-27 19:11:01 -0300

    asd

--- src/main/java/com/example/demo/ChatBotController.java
+++ src/main/java/com/example/demo/ChatBotController.java
@@ -6,20 +6,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+
 public class ChatBotController {
 
     @Autowired
     private MailService mailService;
 
-//    @CrossOrigin(origins = "https://www.grupoinhaut.com")
-    @PostMapping("/user-data")
+    @CrossOrigin(origins = "https://www.grupoinhaut.com", allowCredentials = "true")
+    @PostMapping(value = "/api/user-data", consumes = "application/json")
     public ResponseEntity<String> receiveUserData (@RequestBody UserModel user){
         HttpHeaders headers = new HttpHeaders();
         headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
         headers.add("Pragma", "no-cache");
         headers.add("Expires", "0");
-//        headers.add("Access-Control-Allow-Origin", "https://www.tudominio.com");
+        headers.add("Access-Control-Allow-Origin", "https://www.grupoinhaut.com");
         headers.add("Access-Control-Allow-Credentials", "true");
         headers.add("Vary", "Origin");
 
